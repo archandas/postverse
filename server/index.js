3adf2d33b055b8d03c3e7bb9ccf1ca2ad79a8b60
Archan Das <archandas419@gmail.com>
2025-07-07 17:59:17 +0530

    Production ready config

--- server/index.js
+++ server/index.js
@@ -20,7 +20,7 @@ const PORT = process.env.PORT || 5000;
 const ATLAS_DB = process.env.ATLAS_DB;
 
 app.use(cors({
-    origin: ['http://localhost:5173','postverse-three.vercel.app'],
+    origin: ['http://localhost:5173','https://postverse-three.vercel.app'],
     credentials: true,
 }));
 
